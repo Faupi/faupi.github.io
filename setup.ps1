acc3bba3f6031abe4fbfc99700b168aa6170edfa
Faupi <matej.sp583@gmail.com>
2023-05-30 21:05:02 +0200

    Init

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+choco install -y git --params="'/GitAndUnixToolsOnPath /NoAutoCrlf'"
+
+choco install -y `
+microsoft-windows-terminal 1password vscodium.install imageglass `
+modernflyouts msiafterburner equalizerapo nilesoft-shell start10 `
+firefox `
+telegram steam discord spotify `
+sharex paint.net
+
+# choco install icue --version 4.33.138 -y
+# choco install cura-new --version 5.3.0 -y
+# choco install winbox --version 3.37 -y
+# choco install scrcpy --version 2.0 -y
