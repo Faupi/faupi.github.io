b5646c5747cbf7acd62fca7cd971b88a6e8fd967
Faupi <83234824+Faupi@users.noreply.github.com>
2023-06-20 09:57:13 +0200

    Added shared powershell profile

---
+++ ps-profile.ps1
@@ -0,0 +1,16 @@
+$OH_MY_POSH_CACHED = "$env:POSH_THEMES_PATH\faupi-cached.omp.json"
+
+curl https://faupi.net/faupi.omp.json > "$OH_MY_POSH_CACHED.dl" --silent --max-time 5
+if($LASTEXITCODE -eq 0){
+    # Copy the downloaded theme only if it was downloaded okay
+    Copy-Item -Path "$OH_MY_POSH_CACHED.dl" -Destination "$OH_MY_POSH_CACHED"
+}
+oh-my-posh init pwsh --config "$OH_MY_POSH_CACHED" | Invoke-Expression
+
+function gco {
+  git checkout $args
+}
+
+function gc {
+  git commit $args
+}
